f36c45da22b09519d9a03978941d8ff410c142e6
Danilo Gabriel Bonifacio Bautista <danilo_bonifacio@ucne.edu.do>
2022-05-10 16:44:43 -0400

    Agregar dependencias, corrutinas, implementaciones y demas

--- app/src/main/java/com/BonifacioTech/myapplication/MainActivity.kt
+++ app/src/main/java/com/BonifacioTech/myapplication/MainActivity.kt
@@ -3,7 +3,15 @@ package com.BonifacioTech.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val ocupacionViewModel by viewModels()
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
